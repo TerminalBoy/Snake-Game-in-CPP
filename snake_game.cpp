68d2a7d5daf1134735e78e5daa3eb9f430692c00
Shaz <shazkamil1@gmail.com>
2025-06-08 13:07:34 +0530

    Commit - Added working food system.. bug: screen glitches a little after eating food

--- snake_game.cpp
+++ snake_game.cpp
@@ -64,6 +64,10 @@ class snake { // Will hold Snake information
       move_interval= 1.0f / speed;
     }
     
+    inline int get_max_size(){
+      return max_size;  
+    }
+    
     ~snake();
     
   protected:
@@ -101,30 +105,32 @@ class snake_part{
 class snake_food{
   public:
   
+  
+  float radius;
+  sf::Color color;
+
   sf::Vector2f position;
-  sf::Vector2f temp;
   sf::CircleShape shape;
   
   bool eaten = false;
   
   snake_food(){
-    shape.setFillColor(sf::Color::Blue);
+    shape.setRadius(10.f);
+    color = sf::Color::Red;
   }
-  snake_food(int f_radius){
-    shape.setRadius(f_radius);
-    shape.setFillColor(sf::Color::Blue);
+  snake_food(float f_radius){
   }
   
   void put_food(snake& f_snake, sf::RenderWindow& f_window){
+    srand(time(0));
     if (eaten == true){
       cout<<endl<<"Food Added"<<endl;
       int ran_x;
       int ran_y;
-      sf::Vector2f position;
+      
       bool exit_loop = false;
       
       do{
-        
         
         ran_x = rand() % 35; //(game_window::max_x / 20)
         ran_y = rand() % 25; //(game_window::max_y / 20)
@@ -132,23 +138,21 @@ class snake_food{
         position.x = ran_x * 20; 
         position.y = ran_y * 20;
         
-        
         // make sure food dosent come on snake body
         for (int i = 0; i < f_snake.size; i++){
           // make the logic for not adding food on snake, snake body/part
-          if (   position.x >= f_snake.part[i].position.x
-              && position.x <= (f_snake.part[i].position.x + 20)
-              && position.y >= f_snake.part[i].position.y
-              && position.y <= (f_snake.part[i].position.y + 20)) 
-              exit_loop = true; 
+          if (position != f_snake.part[i].position) exit_loop = true; 
         }
-      } while (!exit_loop);
+      } while (exit_loop == false);
       
       eaten = false;
     }
+    //shape.setPosition(position);
+    //temp = shape.getPosition();
+    //cout<<endl<<temp.x<<" | "<<temp.y<<endl;
+    
     shape.setPosition(position);
-    temp = shape.getPosition();
-    cout<<endl<<temp.x<<" | "<<temp.y<<endl;
+    shape.setFillColor(color);
     f_window.draw(shape);
   }
 };
@@ -231,7 +235,7 @@ void snake::init(){
   update_speed(speed);
   
   for (int i = 0; i < size; i++){
-    part[i].position.x = (20 * (size - i)) - 1;
+    part[i].position.x = (20 * (size - i));
     part[i].position.y = 0.f;
   }
 }
@@ -259,7 +263,7 @@ int main(){
   game_over.title = "Game Over";
   
   snake snk;
-  snk.update_speed(5);
+  snk.update_speed(1);
   snk.size = 5;
   snk.transform(200); // creates snake with its parts  
   
@@ -268,8 +272,12 @@ int main(){
   snake_part::shape.setOutlineColor(sf::Color::White);  
   snake_part::shape.setOutlineThickness(1.f); 
   
-  snake_food food(10.f);
-  food.eaten = true; // to first initialize the food
+  snake_food food;
+  food.position.x = 400;
+  food.position.y = 400;
+  food.color = sf::Color::Red;
+  
+  food.eaten = false; // to first initialize the food
   
   snk.is_right=false;
 
@@ -285,7 +293,10 @@ int main(){
   
   if (!font.loadFromFile("Figtree-VariableFont_wght.ttf")) return -1;
 
-  while(window.isOpen()){    
+  while(window.isOpen()){
+    
+    window.clear(sf::Color::White);
+        
     while (window.pollEvent(window_event)){
       if (window_event.type == sf::Event::Closed) window.close();
     }
@@ -363,7 +374,7 @@ int main(){
       else if (snk.is_up){
         snk.part[0].position.y -= 20.f;
         
-        if (snk.part[0].position.y <= -19)
+        if (snk.part[0].position.y <= -20)
         {snk.part[0].position.y = g_window.max_y - 19;} 
         
         for (int i = 1; i <snk.size; i++){
@@ -374,7 +385,7 @@ int main(){
       else if (snk.is_left){
         snk.part[0].position.x -= 20.f;
         
-        if (snk.part[0].position.x <= -19)
+        if (snk.part[0].position.x <= -20)
         {snk.part[0].position.x = g_window.max_x - 19;}
                 
         for (int i = 1; i <snk.size; i++){
@@ -396,14 +407,15 @@ int main(){
     
     
     // to detect if food is eaten or not 
-    
-    if (snk.part[0].position == food.position){
+    cout<<endl<<snk.part[0].position.x <<"  |  "<<snk.part[0].position.y<<endl;
+    if (snk.part[0].position == food.shape.getPosition()){
       food.eaten = true;
+      snk.update_speed(snk.speed + 1);
+      
+      //if (snk.size > snk.get_max_size() - 2) snk.transform(snk.get_max_size() + 100);
+      snk.size += 1;
+      cout<<endl<<"Food Eated"<<endl;
     }
-    
-    food.put_food(snk, window);
-    
-    
     
     // to detect self collision
     for (int i = 4; i < snk.size; i++){ // Check Self Collision
@@ -458,12 +470,9 @@ int main(){
       }
     }
     
-    window.clear(sf::Color::White);
+    food.put_food(snk, window);
     snk.draw_snake(window);
     window.display();
-    
-    
-    
     
   }
   return 0;
