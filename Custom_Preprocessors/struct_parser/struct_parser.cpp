f92ac97bf5742a82acb3a6dc351131fdb59df6b0
Sk Shaz Kamil <shazkamil1@gmail.com>
2025-11-18 15:17:00 +0530

    remaking the struct_parser

--- Custom_Preprocessors/struct_parser/struct_parser.cpp
+++ Custom_Preprocessors/struct_parser/struct_parser.cpp
@@ -3,82 +3,70 @@
 #include <fstream>
 #include <vector>
 #include <filesystem>
-
+#include <unordered_map>
 // a very temporary code, ill refactor later but i need this for my ecs
 // so i am making it somehow work, no optimizations, only shortcuts for now
 // i am on a hurry for now
 
-bool if_struct_exists(std::string& str) {
+//this program will scan a cpp file and look for ##-START_PARSE-## to start parsing and ##-STOP_PARSE-## to stop parsing
+// this program will look at component structs  and generate all its element's emplace_back() 
+// like <struct_name>::element_name.emplace_back()
 
-  if (str.find("struct") != std::string::npos) return true;
-  return false;
+void remove_char(std::string& str, char ch) {
+  std::string res = "";
+  for (int i = 0; i < str.size(); i++) {
+    if (str[i] != ch) res = res + str[i];
+  }
+  str = res;
+}
 
+std::string strip_struct_name(std::string str){
+  std::size_t pos = str.find("struct");
+  str.erase(pos, pos + 6);
+  
+  pos = str.find("//");
+  if (pos != std::string::npos) str.erase(pos, str.size() - 1);
+  remove_char(str, ' ');
+  remove_char(str, '{');
+
+  return str;
 }
 
 int main() {
-  const std::string START_PARSE = "// ##-START_PARSE-##";
-  const std::string STOP_PARSE = "// ##-STOP_PARSE-##";
-
-  bool start = false;
-  bool stop = false;
+  const std::string START_PARSE = "##-START_PARSE-##";
+  const std::string STOP_PARSE = "##-STOP_PARSE-##";
   
-  std::string struct_name = "";
+  enum class state { on, off };
+  state parsing_state = state::off;
 
-  std::vector<std::string> element_token;// i know its very bad, ill fix it later
+  std::string current_line = "";
 
   std::ifstream target_file("test.cpp");
-  std::string line = "";
-  
-  if (!target_file.is_open()) std::cout << "Error opening file" << std::endl;
 
-  while (std::getline(target_file, line)) {
-  
-    if (start) {
-      if (if_struct_exists(line)) {
-        std::cout << "struct exists\n";
-        int struct_start = line.find("struct");
-        int ix = struct_start + 6;
-        struct_name = "";
+  std::vector<std::string> struct_token; //elements and structs
+  std::vector<std::string> element_token;
+  std::unordered_map<std::size_t, std::size_t> token_link;
 
-        for (int i = ix; i < line.size(); i++) {
-          if (line[i] != ' ' && line[i] != '{') {
-            struct_name = struct_name + line[i];
-          }
-          else if (line[i] == '{') {
-            break;
-          }
-        }
-        std::cout << "struct name :" << struct_name << std::endl;
-      }
-      else if (struct_name != "") {
-        std::string temp_element = "";
-        bool element_start = false;
+  std::size_t struct_counter = 0;
+  std::size_t element_counter = 0;
 
-        for (int i = line.size() - 1; i >= 0; i--) {
-          if (!element_start && line[i] == ';') element_start = true;
-
-          if (element_start) {
-            if (line[i] != ' ') temp_element = std::string(1, line[i]) + temp_element;
-            if (line[i] == ' ') {
-              element_token.push_back(struct_name + "::" + temp_element + ".emplace_back()");
-
-              break;
-            }
-          }
-          
-        }
+  while (std::getline(target_file, current_line)) {
+     
+    if (parsing_state == state::on) {
+      if (current_line.find("struct") != std::string::npos) {
+        struct_token.push_back(strip_struct_name(current_line));
+        struct_counter++;
       }
 
-      
+
     }
     
-    if (line.find(START_PARSE) != std::string::npos) start = true;
-    if (line.find(STOP_PARSE) != std::string::npos) break;
+    
+    
+    if (current_line.find(START_PARSE)) parsing_state = state::on;
+    if (current_line.find(STOP_PARSE)) parsing_state = state::off;
    
   }
-
-  for (int i = 0; i < element_token.size(); i++) {
-    std::cout << element_token[i] << std::endl;
-  }
+  
   return 0;
 }
